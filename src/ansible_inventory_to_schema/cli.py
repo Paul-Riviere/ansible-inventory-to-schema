74b37f3745bb158691036ff53dc48aff7a38eb0e
Paul-Riviere <38280859+Paul-Riviere@users.noreply.github.com>
2024-01-29 10:13:54 +0000

    chore: last commit

--- src/ansible_inventory_to_schema/cli.py
+++ src/ansible_inventory_to_schema/cli.py
@@ -1,5 +1,7 @@
 import click
 from ansible_inventory_to_schema.__version__ import __version__
+from ansible.inventory.manager import InventoryManager
+from ansible.parsing.dataloader import DataLoader
 
 
 @click.group()
@@ -9,8 +11,12 @@ def cli():
 
 
 @click.command()
-def generate():
-    click.echo("Hello World")
+@click.option('-i','--inventory-path', default='.', show_default=True)
+def generate(inventory_path):
+    click.echo("Inventory path: " + inventory_path)
+    dl = DataLoader()
+    im = InventoryManager(loader=dl, sources=[inventory_path])
+    click.echo(im.get_hosts())
 
 
 cli.add_command(generate)
