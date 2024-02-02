262c4fcb5858a2adea036143a2006484c8efb4f2
Paul-Riviere <38280859+Paul-Riviere@users.noreply.github.com>
2024-02-02 23:13:11 +0000

    chore: last commit

--- src/ansible_inventory_to_schema/commands/generate.py
+++ src/ansible_inventory_to_schema/commands/generate.py
@@ -14,19 +14,37 @@ class Generate:
         # plantuml_output_file.write('@startuml\n' + Generate.grapher(im) + '@enduml')
         # plantuml_output_file.close()
 
-        click.echo(im.groups["all"].get_descendants())
-        Generate.graph_group(im.groups["all"])
+        result = Generate.graph_group(im.groups["all"])
+        formated_result = {"groups": list(dict.fromkeys(result["groups"])), "links": list(dict.fromkeys(result["links"]))}
+
+        click.echo(formated_result)
         # for group_name, group in im.groups.items():
         #     if group.get_name() != "all":
         #         click.echo(group.get_name() + " : " + str(group.get_descendants()))
 
 
     @staticmethod
-    def graph_group(group, depth=0):
+    def graph_group(group, depth=0, result={"groups": [], "links": []}):
         depth += 1
         for kid in group.child_groups:
-            click.echo(kid.get_name() + " is descendant of " + group.get_name() + " at depth " + str(depth))
-            Generate.graph_group(kid, depth)
+            click.echo("  "*depth + kid.get_name() + " is descendant of " + group.get_name() + " at depth " + str(depth))
+            temp_result = Generate.graph_group(kid, depth, result)
+            result["groups"] += temp_result["groups"]
+            result["links"] += temp_result["links"]
+
+            if group.get_name() != "all":
+                result["links"].append(group.get_name() + "-->" + kid.get_name())
+        
+        temp_group = ""
+        if group.get_name() != "all":
+            temp_group = 'rectangle "**' + group.get_name() + '**\n--'.encode("unicode_escape").decode("UTF-8")
+            for host in group.hosts:
+                temp_group += "\n".encode("unicode_escape").decode("UTF-8") + host.name
+
+            temp_group += '" as rectangle' + str(1) + "\n"
+            result["groups"].append(temp_group)
+        
+        return result
 
 
     @staticmethod
